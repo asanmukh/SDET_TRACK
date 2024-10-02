2b0a2702b28addd55564f479927f402043f563e0
axs848 <akash.sanmukh@sherwin.com>
2024-10-02 14:21:20 +0530

    Committing Extent report generation: Under the TestBase class created method generation of reports to initialize the extent report under BeforeSuite tag and under AfterSuite tag we are closing the reports to save.

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,11 +1,18 @@
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import utilities.GlobalPropertiesReader;
 
 import java.sql.Driver;
 
 public class TestBase {
+
+    public ExtentReports extentReports;
+    public ExtentSparkReporter extentSparkReporter;
 
     @BeforeMethod
     public void setUp() {
@@ -13,6 +20,18 @@ public class TestBase {
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.get(GlobalPropertiesReader.getPropertyValue("url"));
+    }
+
+    @BeforeSuite
+    public void generationOfReports() {
+        extentReports = new ExtentReports();
+        extentSparkReporter = new ExtentSparkReporter(GlobalPropertiesReader.getPropertyValue("reportLocation"));
+        extentReports.attachReporter(extentSparkReporter);
+    }
+
+    @AfterSuite
+    public void closeReports() {
+        extentReports.flush();
     }
 
 }
