c0021fcc4ea979d78db731b8e49da59e0f01ea7b
axs848 <akash.sanmukh@sherwin.com>
2024-10-05 00:01:49 +0530

    feat(tests) : added clickOnMobilesAndAccessories, clickOnCameras methods in ElectronicsPage class, also added locators in ElectronicsPageLocators interface, added tests in ElectronicsPageTests to check the functionalities of clickOnMobilesAndAccessories and clickOnCameras methods and tested successfully.

--- src/main/java/Pages/ElectronicsPage.java
+++ src/main/java/Pages/ElectronicsPage.java
@@ -4,6 +4,7 @@ import Utilities.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import static Locators.ElectronicsPageLocators.*;
+
 public class ElectronicsPage {
 
     public WebDriver driver;
@@ -16,8 +17,17 @@ public class ElectronicsPage {
         DriverFactory.get().findElement(electronics).click();
     }
 
+    public void clickOnMobilesAndAccessories() {
+        DriverFactory.get().findElement(mobilesAndAccessories).click();
+    }
+
+    public void clickOnCameras() {
+        DriverFactory.get().findElement(cameras).click();
+    }
+
     public void addItemToCart(String itemName) {
         DriverFactory.get().findElement(By.xpath("//*[@class='a-size-medium a-color-base a-text-normal' and text()='" + itemName + "']")).isDisplayed();
         DriverFactory.get().findElement(By.xpath("(//*[text()='" + itemName + "'])[1]/parent::a/parent::h2/parent::div/following-sibling::div/div/div/div/div/div/div/span/div/span/span")).click();
     }
+
 }
--- src/test/java/ElectronicsPageTests.java
+++ src/test/java/ElectronicsPageTests.java
@@ -1,13 +1,30 @@
 import Pages.ElectronicsPage;
-import Pages.HomePage;
 import Utilities.DriverFactory;
 import org.testng.annotations.Test;
 
-public class ElectronicsPageTests {
+public class ElectronicsPageTests extends TestBase {
 
     @Test
     public void addItemToCart(String itemName) {
         ElectronicsPage electronicsPage = new ElectronicsPage(DriverFactory.get());
         electronicsPage.addItemToCart(itemName);
+    }
+
+    @Test
+    public void clickOnElectronics() {
+        ElectronicsPage electronicsPage = new ElectronicsPage(DriverFactory.get());
+        electronicsPage.clickOnElectronics();
+    }
+
+    @Test
+    public void clickOnMobilesAndAccessories() {
+        ElectronicsPage electronicsPage = new ElectronicsPage(DriverFactory.get());
+        electronicsPage.clickOnMobilesAndAccessories();
+    }
+
+    @Test
+    public void clickOnCameras() {
+        ElectronicsPage electronicsPage = new ElectronicsPage(DriverFactory.get());
+        electronicsPage.clickOnCameras();
     }
 }
