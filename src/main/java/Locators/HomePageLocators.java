85c8e009474684baf9e21d461770901daa637d63
axs848 <akash.sanmukh@sherwin.com>
2024-10-13 21:32:45 +0530

    Implemented to run test cases in headless mode using chrome, firefox, and edge drivers. Made to tests in the HomePageTests.

--- src/main/java/Locators/HomePageLocators.java
+++ src/main/java/Locators/HomePageLocators.java
@@ -12,10 +12,11 @@ public interface HomePageLocators {
     By bestSellers = By.xpath("(//*[@class='hmenu-item' and contains(text(),'Best Sellers')])[1]");
     By bestSellersInBagsWalletsAndLuggage = By.xpath("//*[text()='Bestsellers in Bags, Wallets and Luggage']");
     By bestSellersNextPageButton = By.xpath("//*[text()='Bestsellers in Bags, Wallets and Luggage']/parent::div/parent::div/following-sibling::div/div/div/a/span/i[@class='a-icon a-icon-next']");
-    By eighthItemFromBagsBestSellers = By.xpath("//*[@class='a-carousel-heading a-inline-block' and contains(text(),'Bestsellers in Bags, Wallets and Luggage')]/parent::div/parent::div/following-sibling::div//li[4]/div//span/div");
+    By eighthItemFromBagsBestSellers = By.xpath("//*[text()='Bestsellers in Bags, Wallets and Luggage']/parent::div/parent::div//div[@id='gridItemRoot']/div[@id='p13n-asin-index-7']/div//div/a/span/div");
     By eighthItemTitle = By.xpath("//*[@id='productTitle' and contains(text(),'American Tourister Liftoff')]");
     By addItemToCartButton = By.xpath("//*[@title='Add to Shopping Cart']");
     By skipButtonToSkipAddOnOrder = By.xpath("//*[@id='attachSiNoCoverage-announce']");
-    By addedToCartSuccessMessage = By.xpath("//*[@role='status' and contains(text(),'Added to Cart')]");
+    By addedToCartSuccessMessage = By.xpath("//*[@class='a-size-medium-plus a-color-base sw-atc-text a-text-bold' and contains(text(),'Added to Cart')]");
+    By bagsWalletsAndLuggage =By.xpath("//*[text()='Bags, Wallets and Luggage']");
 }
 
